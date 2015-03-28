c19478ff278d6abe8a5f2e465829462cc614eb76
chekun <234267695@qq.com>
2015-03-28 16:40:55 +0800

    everybody should use ends

---
+++ env.go
@@ -0,0 +1,76 @@
+package spore
+
+import (
+	l4g "code.google.com/p/log4go"
+	"database/sql"
+	"errors"
+	"flag"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/yaml.v1"
+	"io/ioutil"
+	"runtime"
+)
+
+var ConfigFile string
+var ConfigEnvironment string
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	l4g.AddFilter("stdout", l4g.INFO, l4g.NewConsoleLogWriter())
+	l4g.AddFilter("logfile", l4g.DEBUG, l4g.NewFileLogWriter("spored.log", false))
+}
+
+func ConfigFlags(f *flag.FlagSet) {
+	f.StringVar(&ConfigFile, "config", "dbconfig.yml", "Configuration file to use.")
+	f.StringVar(&ConfigEnvironment, "env", "development", "Environment to use.")
+}
+
+type Environment struct {
+	Dialect    string `yaml:"dialect"`
+	DataSource string `yaml:"datasource"`
+	Dir        string `yaml:"dir"`
+	TableName  string `yaml:"table"`
+}
+
+func ReadConfig() (map[string]*Environment, error) {
+	file, err := ioutil.ReadFile(ConfigFile)
+	if err != nil {
+		return nil, err
+	}
+
+	config := make(map[string]*Environment)
+	err = yaml.Unmarshal(file, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func GetEnvironment() (*Environment, error) {
+	config, err := ReadConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	env := config[ConfigEnvironment]
+	if env == nil {
+		return nil, errors.New("No environment: " + ConfigEnvironment)
+	}
+
+	if env.DataSource == "" {
+		return nil, errors.New("No data source specified")
+	}
+
+	return env, nil
+}
+
+func GetConnection(env *Environment) (*sql.DB, error) {
+	db, err := sql.Open("mysql", env.DataSource)
+	if err != nil {
+		return nil, fmt.Errorf("Cannot connect to database: %s", err)
+	}
+
+	return db, nil
+}
